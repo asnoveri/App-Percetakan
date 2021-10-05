aed484d250e76421f4c59fffb3468f61c37ea1a9
asnoveri <asnoveri15@gmail.com>
2021-10-05 12:52:01 +0700

    menambahkan smooth scroll

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,16 +1,42 @@
-//  set menu active
-const menus = document.querySelectorAll(".menu");
-for (let i = 0; i < menus.length; i++) {
-  menus[i].addEventListener("click", function () {
-    let current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+let menuSection = document.querySelectorAll(".menu");
+
+for (const m of menuSection) {
+  m.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+
+  document.querySelector(href).scrollIntoView({
+    behavior: "smooth",
+    block: "end",
   });
 }
 
-window.addEventListener("scroll", (event) => {
-  // console.log(window.scrollY);
-  // console.log(document.documentElement.scrollHeight);
-  // console.log(innerHeight);
-  // console.log(innerWidth);
+// for clickable event
+menuSection.forEach((v) => {
+  v.onclick = () => {
+    changeactiv(v);
+  };
 });
+
+function changeactiv(v) {
+  setTimeout(() => {
+    menuSection.forEach((j) => j.classList.remove("active"));
+    v.classList.add("active");
+  }, 300);
+}
+
+// for scrol on top event
+window.onscroll = () => {
+  let mainSection = document.querySelectorAll(" section");
+  mainSection.forEach((v, i) => {
+    let rect = v.getBoundingClientRect().y;
+
+    if (rect < window.innerHeight - 200) {
+      menuSection.forEach((v) => v.classList.remove("active"));
+      menuSection[i].classList.add("active");
+    }
+  });
+};
